1cca58aaf7e2a62e22e4a6f9c952cbb5b214a531
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-14 15:39:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express() // instantiating express server
+
+// all request will have to know that the public folder is where we put out static files
+// public is referring to directories in the folder
+app.use(express.static('public'))
+
+// listening for request
+// get route/path
+// send only take json objects. sendfile takes html
+app.get('/', function(req, res) {
+  res.send('index.html')
+})
+app.get('/about/:name', function(req, res) {
+  res.send(req.params)
+})
+app.get('/faq', function(req, res) {
+  res.send('faq page')
+})
+app.post('/')
+app.listen(3000, function() {
+  console.log('Express is running on port 3000')
+})
