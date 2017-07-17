880510c5c61a82612a2c4afe23006f1cb5351f55
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-17 10:23:34 +0800

    handlebars

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const app = express() // instantiating express server
+const exphbs = require('express-handlebars')
 
 // all request will have to know that the public folder is where we put out static files
 // public is referring to directories in the folder
 app.use(express.static('public'))
 
+// setting dynamic view
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 // listening for request
 // get route/path
 // send only take json objects. sendfile takes html
 app.get('/', function(req, res) {
-  res.send('index.html')
+  res.render('index')
 })
-app.get('/about/:name', function(req, res) {
-  res.send(req.params)
+app.get('/about', function(req, res) {
+  res.render('about')
 })
 app.get('/faq', function(req, res) {
-  res.send('faq page')
+  res.render('faq')
 })
 app.post('/')
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 4000
+
 app.listen(port, function() {
-  console.log('Express is running on port 3000')
+  console.log('Express is running on port ' + port)
 })
